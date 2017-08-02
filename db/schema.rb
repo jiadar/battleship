ad69591102d2ef7552670cfdae2b2a06421d52d3
Ross Capdeville <ross@teamengine.org>
2017-08-01 17:44:51 -0700

    updating config

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151207060813) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boards", force: :cascade do |t|
     t.integer  "user_id",                    null: false
     t.integer  "game_id",                    null: false
